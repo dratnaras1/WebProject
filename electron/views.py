845e19f9b8c0aa8dae6d28aac37a93fb5325f864
Daniel <dratnaras@gmail.com>
2017-03-06 11:18:42 +0000

    lastest feautures implemented

--- electron/views.py
+++ electron/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 # hello
 
 def index(request):
-    return render(request,'electron/index.html')
+    products = Product.objects.order_by('-name')[:5]
+
+    # print(products.length())
+    print(products)
+    context = {'products': products}
+
+    return render(request,'electron/index.html', context)
